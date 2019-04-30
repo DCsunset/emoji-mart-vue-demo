9dcec47144c00f255d2231b6dfac5585990178ab
DCsunset <DCsunset@163.com>
2019-04-30 10:30:11 +0800

    Init

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <Picker />
+  </div>
+</template>
+
+<script>
+import { Picker } from 'emoji-mart-vue-fast'
+import 'emoji-mart-vue-fast/css/emoji-mart.css'
+
+export default {
+  components: {
+    Picker
+  }
+}
+</script>
